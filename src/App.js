152a4fe32cbac7f33109daea9418e06f12a34901
MV-stack <59325046+MV-stack@users.noreply.github.com>
2020-06-20 17:52:44 -0700

    added cards.json, working on jsx in app.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import cards from "./cards.json";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  state = { cards };
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          {this.state.cards.map(card => (<Card key={card.id} name={card.name} image={card.image} />))}
+        </header>
+      </div>
+    )
+  }
+};
 
 export default App;
