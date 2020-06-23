71dc718e1e7775206168b253a3abe85bd7aa88e4
MV-stack <59325046+MV-stack@users.noreply.github.com>
2020-06-22 19:44:00 -0700

    Updates

---
+++ precache-manifest.69df9f618d798b3d21a08b6997981ced.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29e2e68816c47807ba5c9b09b19ae3ab",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "f8c486dfb4a6514838cc",
+    "url": "/clickygame/static/css/main.adaed143.chunk.css"
+  },
+  {
+    "revision": "2834a0ad925b1b61a930",
+    "url": "/clickygame/static/js/2.e6259692.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clickygame/static/js/2.e6259692.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8c486dfb4a6514838cc",
+    "url": "/clickygame/static/js/main.e6bc550d.chunk.js"
+  },
+  {
+    "revision": "792f5ee8a7d816f9a1d4",
+    "url": "/clickygame/static/js/runtime-main.0439b634.js"
+  }
+]);
